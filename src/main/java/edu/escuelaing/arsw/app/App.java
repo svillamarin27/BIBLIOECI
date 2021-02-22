9c011388fcd831a96115ecdacdd2d485b096577a
Sebastian Villamarin R <sebastian.villamarin@mail.escuelaing.edu.co>
2021-02-21 22:24:42 -0500

    nose

--- src/main/java/edu/escuelaing/arsw/app/App.java
+++ src/main/java/edu/escuelaing/arsw/app/App.java
@@ -1,15 +1,15 @@
 package edu.escuelaing.arsw.app;
-import javax.faces.bean.*;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-@ManagedBean(name = "index")
-@ApplicationScoped
+@SpringBootApplication
 public class App 
 {	
 	
     public static void main( String[] args )
     {
+    	SpringApplication.run(App.class, args);
         System.out.println( "Bienvenido a BiblioEci" );
     }
 }
