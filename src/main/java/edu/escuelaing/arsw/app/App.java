e71c7360e234bdc5018e974b067ca88894f6df78
Sebastian Villamarin R <sebastian.villamarin@mail.escuelaing.edu.co>
2021-02-21 19:48:18 -0500

    First commit

---
+++ src/main/java/edu/escuelaing/arsw/app/App.java
@@ -0,0 +1,13 @@
+package edu.escuelaing.arsw.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
