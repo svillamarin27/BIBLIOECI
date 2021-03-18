8fad18f22beb30dba9bd194f4b1b696ac73f03d8
Sebastian Villamarin R <sebastian.villamarin@mail.escuelaing.edu.co>
2021-03-17 22:02:41 -0500

    tablas

--- Tablas.sql
+++ Tablas.sql
@@ -1,5 +1,6 @@
 
 Create table prestamo (
+	disponible boolean not null,
 	idPrestamo int not null,
 	idUsuario int not null,
 	cantidadDias int not null
@@ -8,7 +9,7 @@ Create table prestamo (
 Create table usuario (
 	nombre varchar(30) not null,
 	edad int not null, 
-	cedula int not null, 
+	id int not null, 
 	celular int not null,
 	telefono int not null,
 	direccion varchar(20) not null, 
@@ -16,64 +17,40 @@ Create table usuario (
 );
 
 Create table chat (
-	idUsuario int not null,
+	bot boolean not null,
 	persona boolean not null,
-	idChat int not null, 
-	mensaje varchar(99999) not null
+	idChat int not null 
 );
 
 Create table cliente (
 	cantPrestamo int not null,
-	cantMulta int not null,
-	idUsuario int not null
+	cantMulta int not null
 );
 
 Create table administrador (
-	idUsuario int not null,
-	cantRegistro int not null
+	idUsuario int not null
 );
 
 Create table libro (
 	idPrestamo int not null,
-	idLibro int not null,
+	nombreLibro varchar(30) not null,
 	autor varchar(30) not null, 
 	disponible boolean not null, 
 	categoria varchar(30) not null,
 	editorial varchar(30) not null,
-	idioma varchar(15) not null
+	idioma varchar(15) not null,
+	tipo varchar(30) not null
 );
 
-Create table cientifico(
-	idLibro int not null,
-	volumen int not null
-);
 
-Create table linguistico(
-	idLibro int not null,
-	parte int not null
-);
-
-Create table literatura(
-	idLibro int not null,
-	parte int not null
-);
-
-Create table bibliografico(
-	idLibro int not null,
-	volumen int not null
-);
 
 
 alter table "prestamo" add constraint PK_PRESTAMO primary key(idPrestamo);
-alter table "usuario" add constraint PK_USUARIO primary key(cedula);
+alter table "usuario" add constraint PK_USUARIO primary key(id);
 alter table "chat" add constraint PK_CHAT primary key(idChat);
-alter table "cliente" add constraint PK_CLIENTE primary key(idUsuario);
+alter table "cliente" add constraint PK_CLIENTE primary key(cantPrestamo);
 alter table "administrador" add constraint PK_ADMINISTRADOR primary key(idUsuario);
-alter table "libro" add constraint PK_LIBRO primary key(idLibro);
-alter table "cientifico" add constraint PK_CIENTIFICO primary key(idLibro);
-alter table "linguistico" add constraint PK_LINGUISTICO primary key(idLibro);
-alter table "literatura" add constraint PK_LITERATURA primary key(idLibro);
-alter table "bibliografico" add constraint PK_BIBLIOGRAFICO primary key(idLibro);
+alter table "libro" add constraint PK_LIBRO primary key(nombreLibro);
 
 
 
