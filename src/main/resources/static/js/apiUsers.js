aa513d131c3e16aba67c0c1916d0c35c719d153f
daniel <daniel_vrgs@hotmail.com>
2021-04-04 18:40:29 -0500

    conexion back-front

--- src/main/resources/static/js/apiUsers.js
+++ src/main/resources/static/js/apiUsers.js
@@ -1,29 +1,13 @@
-var apiUsers = (function () {
-    function validateUsers() {
-        var username = document.getElementById("user").value;
-        var password = document.getElementById("pass").value;
-        if (username === "" || password === "") {
-            alert("Todos los campos son requeridos.");
-        } else {
-            const options = {
-                method: 'GET',
-                url: "http://localhost:8080/user/checkusers" + username
-            };
-            axios.request(options).then(function (response) {
-                console.log(response);
-                if (response.data) {
-                    alert("Verdadero");
-                }
-                else {
-                    alert("Falso");
-                }
-            }).catch(function (error) {
-                console.error(error);
-            });
+$("#boton-usuarios").on("click", validateUsers);
 
+function validateUsers() {
+    $.ajax({
+        url: 'http://localhost:8080/user/users',
+        success: function(respuesta) {
+            console.log(respuesta);
+        },
+        error: function() {
+            console.log("No se ha podido obtener la información");
         }
-    }
-    return {
-        validateUsers : validateUsers
-    }
-})();
+    });
+}
