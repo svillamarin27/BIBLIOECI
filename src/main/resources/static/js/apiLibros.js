1aa993f097edf550cfeceec7e3bd8739c4dcd34d
daniel <daniel_vrgs@hotmail.com>
2021-04-08 01:12:08 -0500

    Corrigiendo urls

--- src/main/resources/static/js/apiLibros.js
+++ src/main/resources/static/js/apiLibros.js
@@ -78,7 +78,7 @@ apiLibros = (function(){
     function returnBooks(){
         console.log('Funcion para retornar libros');
         const xhttp = new XMLHttpRequest();
-        xhttp.open('GET', 'http://localhost:8080/book/rentbooks', true);
+        xhttp.open('GET', 'https://biblioeci.herokuapp.com/book/rentbooks', true);
         xhttp.send();
         xhttp.onreadystatechange = function(){
             if(this.readyState == 4 && this.status == 200){
